4713d09c8def2cff1e422035382c05a1d8ecfb7d
Thomas von Mentlen <thomas.vonmentlen@corporatesoftware.ch>
2021-02-08 21:07:48 +0000

    change access level

--- RunRemoteScript.cs
+++ RunRemoteScript.cs
@@ -19,7 +19,7 @@ namespace FunctionApp1
     {
         [FunctionName("RunRemoteScript")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
